00ee2c6302be31e0b925a326d14c19c7c5e78f38
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-29 20:14:13 +0300

    Print OS name

--- bootloader.asm
+++ bootloader.asm
@@ -1,5 +1,34 @@
-loop:
-    jmp loop
+mov ah, 0x0e
+
+mov al, 't'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'r'
+int 0x10
+int 0x10
+
+mov al, 'a'
+int 0x10
+
+mov al, 'v'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'c'
+int 0x10
+
+mov al, 'h'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+jmp $
 
 times 510 - ($ - $$) db 0
 
