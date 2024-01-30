5494be7ddc089ea517724286e1b8eb0ac214ca94
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-30 11:11:36 +0300

    Trying to call main from c code

--- bootloader.asm
+++ bootloader.asm
@@ -1,34 +1,7 @@
-mov ah, 0x0e
+bits 32
 
-mov al, 't'
-int 0x10
-
-mov al, 'e'
-int 0x10
-
-mov al, 'r'
-int 0x10
-int 0x10
-
-mov al, 'a'
-int 0x10
-
-mov al, 'v'
-int 0x10
-
-mov al, 'e'
-int 0x10
-
-mov al, 'c'
-int 0x10
-
-mov al, 'h'
-int 0x10
-
-mov al, 'e'
-int 0x10
-
-jmp $
+extern main
+call main
 
 times 510 - ($ - $$) db 0
 
